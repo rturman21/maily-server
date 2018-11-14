5a755574704cae43d4d39f814bb90e3dc8282edc
Robert Turman <rlt@Roberts-MacBook-Pro.local>
2018-11-13 19:33:24 -0500

    finished authentication

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+//which credentials to return
+
+if (process.env.NODE_ENV === 'production') {
+    //production keys
+    module.exports = require('./prod');
+} else {
+    //dev keys
+    module.exports = require('./dev');
+}
