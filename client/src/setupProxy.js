7c22e1ed08da01f7fc8ccb6f7bdf6c6b986679d6
Robert Turman <rlt@Roberts-MacBook-Pro.local>
2019-01-29 16:20:03 -0500

    Added SendGrid functionality for automated emailing, added additional routing for surveys

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -7,4 +7,5 @@ module.exports = function(app) {
     app.use(proxy('/api/*', {
         target: 'http://localhost:5000'
     }))
+    app.use(proxy("/api/surveys/thanks", { target: "http://localhost:5000" }));
 }
