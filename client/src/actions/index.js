9faa36de9c0ff6fa51e1375d88c053681576a618
Robert Turman <rlt@Roberts-MacBook-Pro.local>
2019-02-13 15:17:58 -0500

    users can now see survey results

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 import {
-    FETCH_USER
+    FETCH_USER,
+    FETCH_SURVEYS
 } from './types';
+
 
 export const fetchUser = () => async dispatch => {
     const res = await axios.get('/api/current_user');
@@ -26,6 +28,15 @@ export const submitSurvey = (values, history) => async dispatch => {
     history.push('/surveys');
     dispatch({
         type: FETCH_USER,
+        payload: res.data
+    })
+}
+
+export const fetchSurveys = () => async dispatch => {
+    const res = await axios.get('api/surveys');
+
+    dispatch({
+        type: FETCH_SURVEYS,
         payload: res.data
     })
 }
